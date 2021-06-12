01a80d50c6d61288cc4061c94adec9face641675
Gemma <gemma@gemmaelsom.com>
2021-06-12 10:49:04 +1000

    Revert "Ran"
    This reverts commit 656037cee5f55e4bb8c0b686b1bc3c8888ba68df.

--- db/migrate/20210611235930_add_user_to_roles.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToRoles < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :roles, :user, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_235930) do
+ActiveRecord::Schema.define(version: 2021_06_11_100107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,8 +48,6 @@ ActiveRecord::Schema.define(version: 2021_06_11_235930) do
     t.string "role"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_roles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -64,5 +62,4 @@ ActiveRecord::Schema.define(version: 2021_06_11_235930) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "roles", "users"
 end
