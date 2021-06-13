9f5dd4232f346bd5688efbb5634bc481fd6c1bf0
Gemma <gemma@gemmaelsom.com>
2021-06-13 16:16:41 +1000

    added users photo to profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,11 +2,14 @@ class ProfileController < ApplicationController
   def show
     @dog = Dog.find_by(user_id: current_user.id)
 
+    @user = current_user
+
     puts "showing dog down"
     puts @dog
     puts @dog.name
     puts @dog.photo.key
     puts @dog.photo
     puts "showing dog up"
+  
   end
 end
