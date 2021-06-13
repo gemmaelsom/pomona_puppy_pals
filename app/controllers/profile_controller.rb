86582cb1e92ad03394c8ae11dbf7cdd370501d5f
Gemma <gemma@gemmaelsom.com>
2021-06-13 16:38:09 +1000

    added option for if there is no dog (profile_controller.rb)

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,14 +2,16 @@ class ProfileController < ApplicationController
   def show
     @dog = Dog.find_by(user_id: current_user.id)
 
-    @user = current_user
+    if (@dog.present?)
+      @user = current_user
 
-    puts "showing dog down"
-    puts @dog
-    puts @dog.name
-    puts @dog.photo.key
-    puts @dog.photo
-    puts "showing dog up"
+      puts "showing dog down"
+      puts @dog
+      puts @dog.name
+      puts @dog.photo.key
+      puts @dog.photo
+      puts "showing dog up"
+    end
   
   end
 end
